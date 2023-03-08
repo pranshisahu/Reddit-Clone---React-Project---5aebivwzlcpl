0e90127167f1d85abe08cca43435e42b960c5a38
Pranshi Sahu <115640741+pranshisahu@users.noreply.github.com>
2023-03-08 14:55:34 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,49 +1,32 @@
-import React from "react";
-import "./Header.css";
+import React from 'react'
+import './Header.css'
 
-import { useAuth0 } from "@auth0/auth0-react";
+
 function Header() {
-  const { loginWithRedirect,logout,isAuthenticated } = useAuth0();
+    
 
-
- 
-  return (
-    <div className="header">
-      <div className="header__left">
-        <ul>
-          <li>
-            <a href="/r/popular" className="active">
-              Popular
-            </a>
-          </li>
-          <li>
-            <a href="/r/hot">Hot</a>
-          </li>
-          <li>
-            <a href="/r/rising">Rising</a>
-          </li>
-          <li>
-            <a href="/r/controversial">Controversial</a>
-          </li>
-          <li>
-            <a href="/r/gilded">Glided</a>
-          </li>
-        </ul>
-      </div>
-      <div className="header__right">
-        <i className="fas fa-bell"></i>
-        {/* <img src=""  alt="img"/> */}
-        <div className="header__user">
-       {isAuthenticated ? <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
-       : <button onClick={() => loginWithRedirect()}>Log In</button> }
-          {/* <i className="fas fa-caret-down"></i> */}
+    return (
+        <div className='header'>
+            <div className='header_left'>
+                <ul>
+                    <li><a href="/r/popular" class="active">Popular</a></li>
+                    <li><a href="/r/hot">Hot</a></li>
+                    <li><a href="/r/rising">Rising</a></li>
+                    <li><a href="/r/controversial">controversial</a></li>
+                    <li><a href="/r/gilded">Gilded</a></li>
+                </ul>
+            </div>
+            <div className='header_right'>
+                <i className='fas fa-bell'></i>
+                <img src='' />
+                <div className='header_user'>
+                <span>Pranshi sahu</span>
+                     <i className='fas fa-caret-down'></i>
+                    
+             
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
-
 export default Header;
-
-
-     
