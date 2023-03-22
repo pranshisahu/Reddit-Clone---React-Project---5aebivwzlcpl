8e6b547a9c113e95ca02b5e70dc8b569a8e3d627
Pranshi Sahu <115640741+pranshisahu@users.noreply.github.com>
2023-03-22 11:19:19 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import Header from './Header'
-import Posts from './Posts'
+import Header from '../header/Header'
 import './Main.css'
-
+import Post from '../post/Post'
+// import SideNav from "../sidenav/SideNav"
 function Main() {
-    return(
-        <div className='main'>
-            <Header />
-            <Posts/>
-        </div>
-    )
+  return (
+    <div className="main">
+      {/* <SideNav /> */}
+      <Header />
+      <Post />
+    </div>
+  )
 }
 
-export default Main;
+export default Main
