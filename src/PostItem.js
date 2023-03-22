9c929b7b7fb4bb84beac107c137a38b2e4fa5c68
Pranshi Sahu <115640741+pranshisahu@users.noreply.github.com>
2023-03-22 11:39:41 +0530

    Update PostItem.js

--- src/PostItem.js
+++ src/PostItem.js
@@ -1,32 +1,55 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './PostItem.css'
-
+import { useAuth0 } from "@auth0/auth0-react";
 function PostItem(props) {
-    const { title, upvote, image, user, subreddit, comment_count} = props.post
-    return (
-        <div className='post'>
-             <div className='post_left'>
-                <i className='fas fa-caret-up'></i>
-                     <span>{upvote}</span>
-                <i className='fas fa-caret-down'></i>
-             </div>
-             <div className='post_center'>
-                <img src ={image} />
-             </div>
-             <div className='post_right'>
-                <h3><a href={`/r/${subreddit}/{title}`}>{title}</a></h3>
-                <span className='post_info'>
-                    submitted an hour ago by 
-                    {" "}<a href={`/u/${user}`}>{user}</a>{" "}
-                    to 
-                    {" "}<a href={`/r/${subreddit}`}>{subreddit}</a>
-                </span>
-                <p className='post_info'>
-                    {comment_count} comments | share | save | hide | report
-                </p>
-             </div>
-        </div>
-    )
+  const {title, upvote, image, user, subreddit, comment_count} = props.post;
+  const { isAuthenticated } = useAuth0();
+  const [like,setLike]=useState(false);
+  const [dislike, setDislike]=useState(false);
+ const [vote,setVote]=useState(upvote);
+   
+     
+    const upClicked=()=>{
+       setVote(vote+1);
+       setLike(true);
+       setDislike(false);
+    }
+    const downClicked=()=>{
+     if(vote <= 0){
+      setDislike(true);
+      setLike(false);
+     }
+     else{
+      setVote(vote-1);
+      setDislike(true);
+      setLike(false);
+     }
+  }
+  return (
+    <div className="post">
+       <div className="post__left">
+       <button className='up' onClick={upClicked} disabled={like || !isAuthenticated}><i className="fas fa-caret-up"></i> </button>
+        <span>{vote}</span>
+        <button className='up' onClick={downClicked} disabled={dislike || !isAuthenticated}>  <i className="fas fa-caret-down"></i></button>
+       </div>
+       <div className="post__center">
+          <img src={image} alt="img"/>
+       </div>
+       <div className="post__right">
+         <h3><a href={`/r/${subreddit}/${title}`}>{title}</a></h3>
+         <span className="post__info">
+            submitted an hour ago by
+           {" "}<a href={`/u/${user}`}>{user}</a>{" "}
+            to 
+           {" "}<a href={`/r/${subreddit}`}>{subreddit}</a>
+         </span>
+         <p className="post__info">
+            {comment_count} comments | share | save | hide | report
+         </p>
+       </div>
+    </div>
+  )
 }
 
-export default PostItem;
+export default PostItem
+
